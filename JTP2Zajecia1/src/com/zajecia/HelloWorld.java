28200bc88de50cab406488f66b8f89205999083f
Marcin <Marcin@Marcin-PC2>
2012-10-06 19:49:08 +0200

    -

--- JTP2Zajecia1/src/com/zajecia/HelloWorld.java
+++ JTP2Zajecia1/src/com/zajecia/HelloWorld.java
@@ -1,9 +1,14 @@
 package com.zajecia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloWorld
 {
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
 	public static void main(String[] args)
 	{
-		System.out.println("Hello aaworld");
+		logger.info("Hello world");
 	}
 }
